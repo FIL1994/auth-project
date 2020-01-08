e5c96fc8821e7902300cb0ac6b820524584e4a30
Philip Van Raalte <phil.vanraalte@gmail.com>
2020-01-07 20:51:33 -0500

    added app_user notify

---
+++ docker/postgres/2-triggers.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION notify_event() RETURNS TRIGGER AS $$
+    DECLARE
+        record RECORD;
+        payload JSON;
+    BEGIN
+        IF (TG_OP = 'DELETE') THEN
+            record = OLD;
+        ELSE
+            record = NEW;
+        END IF;
+
+        payload = json_build_object(
+            'table', TG_TABLE_NAME,
+            'action', TG_OP,
+            'data', row_to_json(record)
+        );
+
+        PERFORM pg_notify('events', payload::text);
+
+        RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER notify_user_event
+    AFTER INSERT OR UPDATE OR DELETE ON app_user
+    FOR EACH ROW EXECUTE PROCEDURE notify_event();
+
+INSERT INTO app_user (email, password, name) VALUES ('test', 'pass', 'name');
+
